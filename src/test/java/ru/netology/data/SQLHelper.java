e2ab1a2cd5c9f088a0da2bd421fd8d986fbeb1da
Krideinside <kride2009@yandex.ru>
2023-09-09 16:37:23 +0300

    Code added

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -1,39 +1,43 @@
 package ru.netology.data;
 
 import lombok.SneakyThrows;
+import lombok.val;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
-import javax.management.Query;
-import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class SQLHelper {
-    private static final QueryRunner runner = new QueryRunner();
+    private static QueryRunner runner = new QueryRunner();
 
-    private SQLHelper() {
-    }
 
-    private static Connection getConn() throws SQLException {
-        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
-    }
-
-//    @SneakyThrows
-//    public static DataHelper.VerificationCode getVerificationCode() {
-//        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
-//        var conn = getConn();
-//        var code = runner.query(conn, codeSQL, new ScalarHandler<String>());
-//        return new DataHelper.VerificationCode(code);
-//        }
+    private static String url = System.getProperty("db.url");
+    private static String user = System.getProperty("db.user");
+    private static String password = System.getProperty("db.password");
 
     @SneakyThrows
-    public static void cleanDatabase() {
-        var connection = getConn();
-        runner.execute(connection, "DELETE FROM auth_codes");
-        runner.execute(connection, "DELETE FROM card_transactions");
-        runner.execute(connection, "DELETE FROM cards");
-        runner.execute(connection, "DELETE FROM users");
+    public static void cleanBD() {
+        val deletePaymentEntity = "DELETE FROM payment_entity ";
+        val runner = new QueryRunner();
+        val conn = DriverManager.getConnection(url, user, password);
+        {
+            runner.update(conn, deletePaymentEntity);
+        }
+    }
+
+    @SneakyThrows
+    public static String getPaymentStatus() {
+        String statusSQL = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        return getStatus(statusSQL);
+    }
+
+    @SneakyThrows
+    private static String getStatus(String query) {
+        val runner = new QueryRunner();
+        val conn = DriverManager.getConnection(url, user, password);
+        {
+            String status = runner.query(conn, query, new ScalarHandler<String>());
+            return status;
+        }
     }
 }
-
